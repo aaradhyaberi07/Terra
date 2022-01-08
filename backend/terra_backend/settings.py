c9f80cc7dda29e5c282348c8e776eb99d8b8c724
aaradhyaberi07 <64523034+aaradhyaberi07@users.noreply.github.com>
2022-01-08 11:16:14 +0530

    permissions, exceptions made and settings db updated for migrations

--- backend/terra_backend/settings.py
+++ backend/terra_backend/settings.py
@@ -83,30 +83,30 @@ WSGI_APPLICATION = 'terra_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.mysql',
-#             'NAME': os.getenv('DB_NAME'),
-#             'USER': os.getenv('DB_USER'),
-#             'PASSWORD': os.getenv('DB_PASSWORD'),
-#             'HOST': os.getenv('DB_HOST'),
-#             'PORT': os.getenv('DB_PORT'),
-#             'OPTIONS': {
-#                 'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-#             }
-#         }
-#     }
-# FOR HEROKU
-db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES = {
-        'default': db_from_env
-        #'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        #'NAME': os.getenv('DB_NAME'),
-        #'USER': os.getenv('DB_USER'),
-        #'PASSWORD': os.getenv('DB_PASSWORD'),
-        #'HOST': os.getenv('DB_HOST'),
-        #'PORT': os.getenv('DB_PORT'),
-}
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT'),
+            'OPTIONS': {
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+            }
+        }
+    }
+# FOR HEROKU
+# db_from_env = dj_database_url.config(conn_max_age=600)
+# DATABASES = {
+#         'default': db_from_env
+#         #'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         #'NAME': os.getenv('DB_NAME'),
+#         #'USER': os.getenv('DB_USER'),
+#         #'PASSWORD': os.getenv('DB_PASSWORD'),
+#         #'HOST': os.getenv('DB_HOST'),
+#         #'PORT': os.getenv('DB_PORT'),
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
