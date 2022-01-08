11bf2b45abfad2a38deec83d36bae7462e154be6
aaradhyaberi07 <64523034+aaradhyaberi07@users.noreply.github.com>
2022-01-08 14:42:10 +0530

    deploy contracts updated

--- frontend/migrations/2_deploy_contracts.js
+++ frontend/migrations/2_deploy_contracts.js
@@ -1,5 +1,13 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var Notice = artifacts.require("../contracts/Notice.sol");
+var Insurance = artifacts.require("../contracts/Insurance.sol");
+var supplychain = artifacts.require("../contracts/SupplyChain.sol");
+var Coin = artifacts.require("../contracts/TerraCoin.sol");
+var erc20 = artifacts.require("../contracts/ERC20.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+module.exports = function (deployer) {
+	deployer.deploy(Notice);
+	deployer.deploy(Insurance);
+	deployer.deploy(supplychain);
+	deployer.deploy(erc20);
+	deployer.deploy(Coin);
 };
